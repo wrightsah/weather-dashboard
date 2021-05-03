faaa3a3730bb073ed848a4a1f2909a9678c87ca5
Stephen Wright <wrightsah@icloud.com>
2021-05-03 14:25:13 -0400

    set up basic card template

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log('JS file connected!');
+
+// 6e36cdc576966b7b423bd4e21d54c38c
+
+// api.openweathermap.org/data/2.5/forecast?id=524901&appid={API key}
+
+// todo: get api call object to come through for current city
+
+const weatherCall = api.openweathermap.org/data/2.5/forecast?id=524901&appid=6e36cdc576966b7b423bd4e21d54c38c
+
+console.log(weatherCall);
